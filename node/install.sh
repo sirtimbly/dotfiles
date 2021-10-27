2b1d45a95a6c1abab88e8f06c59365c192d732e2
Tim Bendt <sirtimbly@gmail.com>
2021-10-27 09:11:38 -0600

    adding mackup config and a few more plugins

--- node/install.sh
+++ node/install.sh
@@ -4,4 +4,7 @@ then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 fi
 
+# Global Installs
+npm i -g http-server http-server-spa hexo commitizen typescript
+
 exit 0
--- python/install.sh
+++ python/install.sh
@@ -5,4 +5,5 @@ pipx install granularaccountmap
 # urllib3 has to be pinned
 pipx install --pip-args "--index-url https://artifactory.encirca.pioneer.com/artifactory/api/pypi/pypi-virtual/simple urllib3==1.23" onelogin-granular-cli
 pipx install credstash
+pipx install pygments
 exit 0
