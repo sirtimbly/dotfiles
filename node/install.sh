8be710460ae0e1da590c47276afa6088afba6b80
Tim Bendt <timbendt@granular.ag>
2021-10-29 12:11:07 -0600

    add npkill

--- node/install.sh
+++ node/install.sh
@@ -1,10 +1,10 @@
 # Install NVM
-if  ! test -d ~/.nvm 
+if  ! test -d ~/.nvm
 then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 fi
 
 # Global Installs
-npm i -g http-server http-server-spa hexo commitizen typescript
+npm i -g http-server http-server-spa hexo commitizen typescript npkill
 
 exit 0
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,4 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 alias y="yarn"
 alias g="git"
+alias mux="tmuxinator"
