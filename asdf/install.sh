cae27ea584bfe3a2d1f24350cb52af00717ff709
Tim Bendt <timbendt@granular.ag>
2025-07-17 14:32:07 -0400

    switch to mise isntead of asdf

--- asdf/install.sh
+++ asdf/install.sh
@@ -1,23 +1,23 @@
-echo "Checking if asdf is installed."
-if ! test -d ~/.asdf
-then
-  sh -c "$(git clone https://github.com/asdf-vm/asdf.git --depth 1 --branch v0.14.0 --single-branch ~/.asdf)" "" --keep-zshrc
-  . "$HOME/.asdf/asdf.sh"
-fi
-echo "Checking for asdf plugins."
-PLUGINS=$(cat "$HOME/.tool-versions" | cut -d" " -f1);
-for PLUGIN in $PLUGINS; do
-  echo "Adding plugin $PLUGIN";
-  asdf plugin add "$PLUGIN";
-  status=$?;
-  if [ $status -ne 0 ] && [ $status -ne 2 ]; then
-    echo "Install failed with status $status";
-    exit $status;
-  elif [ $status -eq 2 ]; then
-    echo "Install returned status 2, continuing...";
-  else
-    echo "Install succeeded";
-  fi;
-done
-asdf install
-exit 0
+# echo "Checking if asdf is installed."
+# if ! test -d ~/.asdf
+# then
+#   sh -c "$(git clone https://github.com/asdf-vm/asdf.git --depth 1 --branch v0.14.0 --single-branch ~/.asdf)" "" --keep-zshrc
+#   . "$HOME/.asdf/asdf.sh"
+# fi
+# echo "Checking for asdf plugins."
+# PLUGINS=$(cat "$HOME/.tool-versions" | cut -d" " -f1);
+# for PLUGIN in $PLUGINS; do
+#   echo "Adding plugin $PLUGIN";
+#   asdf plugin add "$PLUGIN";
+#   status=$?;
+#   if [ $status -ne 0 ] && [ $status -ne 2 ]; then
+#     echo "Install failed with status $status";
+#     exit $status;
+#   elif [ $status -eq 2 ]; then
+#     echo "Install returned status 2, continuing...";
+#   else
+#     echo "Install succeeded";
+#   fi;
+# done
+# asdf install
+# exit 0
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,2 +1,3 @@
 # autoload colors && colors
 
+export WTF_GITLAB_TOKEN="$GITLAB_TOKEN"
